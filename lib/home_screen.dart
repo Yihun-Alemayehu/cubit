2abcd9822727aa3e1b0a8e8cc7a7b10202cc3fb3
Yihun Alemayehu <yankure01@gmail.com>
2024-01-12 22:19:46 +0300

    fdb-06

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,35 +17,53 @@ class _HomePageState extends State<HomePage> {
         title: const Text('C O U N T E R'),
         centerTitle: true,
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You pushed the button counter times'),
-            BlocBuilder<CounterCubit, CounterState>(
-              builder: (context, state) {
-                return Text('${state.counter}');
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    context.read<CounterCubit>().increment();
-                  },
-                  child: const Text('Increment'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    // context.read<CounterCubit>().decrement();
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                  },
-                  child: const Text('Decrement'),
-                ),
-              ],
-            )
-          ],
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.isIncremened == true) {
+            // Scaffold.of(context).showSnackBar();
+            const SnackBar(
+              elevation: 10,
+              backgroundColor: Colors.black,
+              content: Text('Incremented'),
+              duration: Duration(milliseconds: 700),
+            );
+          } else if (state.isIncremened == false) {
+            const SnackBar(
+              content: Text('Decremented'),
+              duration: Duration(milliseconds: 300),
+            );
+          }
+        },
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('You pushed the button counter times'),
+              BlocBuilder<CounterCubit, CounterState>(
+                builder: (context, state) {
+                  return Text('${state.counter}');
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      context.read<CounterCubit>().increment();
+                    },
+                    child: const Text('Increment'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // context.read<CounterCubit>().decrement();
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    child: const Text('Decrement'),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
