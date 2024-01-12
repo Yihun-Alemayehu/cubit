dd294dc2d582a6cdfc11c2ce77d72f359a170044
Yihun Alemayehu <yankure01@gmail.com>
2024-01-12 22:45:05 +0300

    fdb-07

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:bloc_concepts_2/cubit/counter_cubit.dart';
-import 'package:bloc_concepts_2/home_screen.dart';
+import 'package:bloc_concepts_2/business/cubit/counter_cubit.dart';
+import 'package:bloc_concepts_2/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_concepts_2/cubit/counter_cubit.dart';
+import 'package:bloc_concepts_2/business/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
