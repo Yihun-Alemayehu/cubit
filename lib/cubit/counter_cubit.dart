037622bf3342fd331abffd3fb90e0e512183588d
Yihun Alemayehu <yankure01@gmail.com>
2024-01-12 21:26:50 +0300

    fdb-04

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,59 @@ void main() {
     BlocProvider(
       create: (context) => CounterCubit(),
       child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: HomePage(),
       ),
     ),
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Counter'),
+        title: const Text('C O U N T E R'),
+        centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You pushed the button counter times'),
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text('${state.counter}');
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    context.read<CounterCubit>().increment();
+                  },
+                  child: const Text('Increment'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // context.read<CounterCubit>().decrement();
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  child: const Text('Decrement'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
